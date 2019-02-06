e0d51821085414d76131a0e6f7e1e2798401c39d
Marc Smith <mr.marcsmith@me.com>
2019-02-05 23:50:50 -0700

    added street

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+## PCAddressFormatter Changelog
+
+### master
+[Full Changelog](https://github.com/panda-clouds/address-formatter/compare/2.0.0...master)
+
+### 0.2.0
+
+- added the "street" property of PCGeocoder
--- src/PCAddressPreview.js
+++ src/PCAddressPreview.js
@@ -60,7 +60,6 @@ class PCAddressPreview  {
 
 		const shortState = PCAddressFormatter.state(result.address.state);
 
-		const geoCoderStreet = result.address.streetNumber + ' ' + result.address.streetName;
 
 
 		const cache = new Parse.Object("PCAddress");                        // convert "Arizona" to "AZ"
@@ -69,8 +68,9 @@ class PCAddressPreview  {
 		if(this.nickname) cache.set("name",this.nickname);
 		cache.set("geoPoint",geo);                                          // (33.6025,-112.02269)
 		cache.set("streetNumber",result.address.streetNumber);              // "2753"
-		cache.set("streetName",result.address.streetName);                  // "E Windrose Dr"
-		if(this.street !== geoCoderStreet) cache.set("inputStreet",this.street);
+		cache.set("streetName",result.address.streetName); 
+		cache.set("street",result.address.street);                  // "E Windrose Dr"
+		if(this.street !== result.address.street) cache.set("inputStreet",this.street);
 		cache.set("city",result.address.city);                              // "Phoenix"
 		if(this.city !== result.address.city) cache.set("inputCity",this.city);
 		cache.set("county",result.address.county)                           // "Maricopa"
